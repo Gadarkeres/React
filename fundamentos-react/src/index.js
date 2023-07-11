f9b3bb1845df871baa07d3d9f2eafbb6d3d0987f
Matheus Rafael <matheusrafael77@hotmail.com>
2023-07-11 17:26:02 -0300

    pratica

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
+
+import Primeiro from './componentes/basicos/Primeiro'
+
+ReactDOM.render(
+        <div>
+            
+        <Primeiro></Primeiro>
+        </div>
+        
+    ,
+    document.getElementById('root')
+)
