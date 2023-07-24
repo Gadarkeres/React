2cdc78e70e48c92e449f5f8d5515a7b106a528c8
Matheus Rafael <matheusrafael77@hotmail.com>
2023-07-24 15:29:05 -0300

    Create FirstComponent

---
+++ React-curso-novo/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FirstComponent = () => {
+    return(
+        <div>
+            <h1>Primeiro componente</h1>
+        </div>
+        /* JSX DEVEM SER ENCAPSULADOS */
+    )
+
+
+}
+
+export default FirstComponent;
+
