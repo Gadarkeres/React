24781544ec4c17a0b0276f0c3429dff8399248f2
Matheus Rafael <matheusrafael77@hotmail.com>
2023-07-26 14:59:51 -0300

    condicional final

--- React-curso-novo/secao3/src/components/ConditionalRender.js
+++ React-curso-novo/secao3/src/components/ConditionalRender.js
@@ -2,6 +2,8 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [x] = useState (false);
+    
+    const[name, setName] = useState ("Matheus");
   
   
   
@@ -10,6 +12,15 @@ const ConditionalRender = () => {
         <h1>isso será exibido?</h1>
         {x && <p>se x for true, sim!</p>}
         {!x && <p>agora é falso</p>}
+        <h1>IF ternário</h1>
+        {name === "João" ? (
+           <p> o nome é João!</p>
+        ) : (
+            <p>nome não encontrado!</p>
+        )}
+       <div>
+       <button onClick={ () => setName("João")}>Clique aqui!</button>
+       </div>
     </div>
   )
 }
