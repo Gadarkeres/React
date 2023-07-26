a9c5f325e9c0a79ba6c5e640a1be461554d16889
Matheus Rafael <matheusrafael77@hotmail.com>
2023-07-26 14:22:41 -0300

    list render with map and key

--- React-curso-novo/secao3/src/components/ListRender.js
+++ React-curso-novo/secao3/src/components/ListRender.js
@@ -2,12 +2,25 @@ import { useState } from "react"
 
 
 const ListRender = () => {
-    const [list] = useState(["Matheus", "Pedro", "Josias"])
+    const [list] = useState(["Matheus", "Pedro", "Josias", "Maria"]);
+
+    const [users] = useState([
+        {id: 13221, name : "Matheus", age:31},
+        {id: 123123, name : "João", age:51},
+        {id: 42322, name : "Pedro", age:44},
+
+    ])
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}> {item}</li>
+            ))}
+        </ul>
+
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name} - idade: {user.age}</li>
             ))}
         </ul>
     </div>
