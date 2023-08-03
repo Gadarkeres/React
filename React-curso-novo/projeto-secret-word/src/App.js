1f8feed94a05ecf7de76b19d3b4fd2707e0e1cf9
Matheus Rafael <matheusrafael77@hotmail.com>
2023-08-03 15:48:55 -0300

    dassads

--- React-curso-novo/projeto-secret-word/src/App.js
+++ React-curso-novo/projeto-secret-word/src/App.js
@@ -14,8 +14,6 @@ import End from './components/End';
 
 
 
-
-
 const stages = [
   {id: 1, name:"começo"},
   {id: 2, name:"Game"},
@@ -30,9 +28,49 @@ function App() {
   const [gameStage, setGameStage] = useState (stages[0].name);
   
   // pegandos os dados da variavel wordslist e passar para ''words''
+
+  const[pickedWord, setpickedWord] = useState("")
+  const[pickedCategory, setpickedCategory] = useState("")
+  const [letters, setLetters] = useState("")
   const[Words] = useState(wordsList)
-   //começa o jogo de palavras
+
+  const[guessedLetters, setguessedLetters] = useState("")
+  const[wrongLetters, setwrongLetters] = useState("")
+  const[guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(3)
+  
+  const pickWordAndCategory = () =>{
+   // categoria
+    const categories = Object.keys(Words)
+    const category = categories[Math.floor(Math.random()* Object.keys(categories).length)]
+
+    console.log(category)
+    // palavra
+
+      const word = Words[category]
+      [Math.floor(Math.random() * Words[category].length)]
+
+      console.log(word)
+      return{word, category}
+  }
+  //começa o jogo de palavras
    const startgame = () =>{
+    // pegar letras e pegar categoria
+
+   const  {word, category} = pickWordAndCategory();
+
+   //creando um array das letras
+    let wordLetters = word.split("")
+    wordLetters = wordLetters.map((l) => l.tolowerCase())
+
+    console.log(word, category)
+    
+    setpickedWord(word);
+    setpickedCategory(category);
+    setLetters(wordLetters);
+
+
+    
     setGameStage(stages[1].name)
    }
 
@@ -50,7 +88,7 @@ function App() {
     <div className="App">
        {/* se o estagio do jogo for ''começo'' > tela inicial */}
     {gameStage === 'começo' &&  <Telainicial startgame={startgame} /> }
-    {gameStage === 'Game' &&  <Game verifyLetter={verifyLetter}/> }
+    {gameStage === 'Game' &&  <Game verifyLetter={verifyLetter} pickedWord={pickedWord} pickedCategory = {pickedCategory} letters = {letters} guessedLetters={guessedLetters} wrongLetters={wrongLetters} guesses={guesses} score={score}/> }
     {gameStage === 'End' &&  <End retry={retry}/>}
     
     </div>
--- React-curso-novo/projeto-secret-word/src/components/Game.js
+++ React-curso-novo/projeto-secret-word/src/components/Game.js
@@ -1,10 +1,38 @@
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinha e palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span> {pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem xxx {guesses}(s)</p>
+      <div className="wordContainer">
+       {letters.map((letter, i) => (
+        guessedLetters.includes(letter) ?(
+          <span key={i} className='letter'></span>
+        ): (
+          <span key={i} className="blancksquare"></span>
+        )
+       ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras ja utilizadas:</p>
+        {wrongLetters.map ((letter, i ) =>{
+          <span key={i}>{letter}</span>
+        })}
+      </div>
     </div>
   )
 }
