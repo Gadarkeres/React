962fdf4aba35cefed93e6e00cd3fdd05d7dc30cf
Matheus Rafael <matheusrafael77@hotmail.com>
2023-08-04 16:26:01 -0300

    ok

---
+++ projeto-email/src/App.js
@@ -0,0 +1,21 @@
+import Headertittle from "./components/Headertittle"
+import Mid from "./components/Mid"
+import InputandButton from "./components/InputandButton";
+
+
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+    <Headertittle/>
+    <Mid/>
+    <InputandButton/>
+    </div>
+   
+     
+  );
+}
+
+export default App;
